139bfbcd97db5386542a479b5d23c3be881af8ff
crydalch <crydalch@b0b181a8-020e-11df-8f7b-bfe16de6f99b>
2010-06-12 00:05:49 +0000

    Added Seth's code for animated static objects.

--- houdini/build_linux.sh
+++ houdini/build_linux.sh
@@ -1,18 +1,13 @@
 #!/bin/csh -f
 
 set INCLUDEBULLET  = /usr/local/include
+set BULLET_LIB = /usr/local/lib
 
-set LIBBULLET = /usr/local/lib
-
-hcustom -g -I$INCLUDEBULLET \
-         \
-         -L$LIBBULLET \
-         -lBulletDynamics \
-         -lBulletCollision \
-         -lLinearMath \
-         src/SIM_SnowSolverBullet.cpp
-
-
+hcustom -I$INCLUDEBULLET -L$BULLET_LIB -lBulletDynamics -lBulletCollision -lLinearMath src/SIM_SnowSolverBullet.cpp
+         
+#set BULLET_SRC=/dist/bullet/bullet-2.76-r1967/src
+#set BULLET_LIBS=/dist/bullet/bullet-2.76-r1967/lib
+#set BULLET_LIBS=/usr/local/lib
 
 # this is an example line for compiling Bullet for linux.
 # please go here for more info: http://odforce.net/wiki/index.php/BulletPhysicsRBDSolverDOP
@@ -21,3 +16,6 @@ hcustom -g -I$INCLUDEBULLET \
 
 #hcustom -g -i . -I ../../bullet-current/src -l LibBulletDynamics -l LibBulletCollision -l LibLinearMath -L  ../../bullet-current/src/BulletDynamics/  -L  ../../bullet-current/src/BulletCollision/  -L ../../bullet-current/src/LinearMath/ SIM_SolverBullet.cpp
 
+#hcustom -I$BULLET_SRC -L$BULLET_LIBS -lBulletCollision -lBulletDynamics -lLinearMath src/SIM_SnowSolverBullet.cpp
+
+#hcustom -I$BULLET_SRC -L$BULLET_LIBS -lBulletSoftBody -lBulletCollision -lBulletDynamics -lLinearMath src/SIM_SnowSolverBullet.cpp
--- houdini/src/SIM_SnowSolverBullet.h
+++ houdini/src/SIM_SnowSolverBullet.h
@@ -1,4 +1,3 @@
-
 #ifndef __SIM_SnowSolverBullet_h__
 #define __SIM_SnowSolverBullet_h__
 
@@ -9,9 +8,9 @@
 #include <vector>
 #include <string>
 
-#define SIM_NAME_SOURCEOBJECTS	"sourceobjects"
+#define SIM_NAME_SOURCEOBJECTS  "sourceobjects"
 #define SIM_NAME_GRAVITY "gravity"
-#define SIM_NAME_BOUNCE	"bounce"
+#define SIM_NAME_BOUNCE "bounce"
 #define SIM_NAME_FRICTION "friction"
 #define SIM_NAME_ERP "erp"
 #define SIM_NAME_CFM "cfm"
@@ -35,38 +34,38 @@ class sim_btRigidBody : public btRigidBody
 {
 
 public:
-	
-	btAlignedObjectArray<btPersistentManifold*> m_manifolds;
-	int houObjectId;
-	
-	
-	// CONSTRUCTORS
-	sim_btRigidBody( const btRigidBody::btRigidBodyConstructionInfo& constructionInfo ) : btRigidBody( constructionInfo )
-	{
-		houObjectId = -1;
-	}  // CONSTRUCTOR
-	
-	sim_btRigidBody( btScalar mass, btMotionState* motionState, btCollisionShape* collisionShape, const btVector3& localInertia=btVector3(0,0,0) )
-		: btRigidBody( mass, motionState, collisionShape, localInertia )
-	{
-		houObjectId = -1;
-	}  // CONSTRUCTOR
-	
-	
-	static const sim_btRigidBody* upcast(const btCollisionObject* colObj)
-	{
-		if ( colObj->getInternalType() == btCollisionObject::CO_RIGID_BODY )
-			return (const sim_btRigidBody*)colObj;
-		return 0;
-	}  // upcast()
-	
-	static sim_btRigidBody*	upcast(btCollisionObject* colObj)
-	{
-		if ( colObj->getInternalType() == btCollisionObject::CO_RIGID_BODY )
-			return (sim_btRigidBody*)colObj;
-		return 0;
-	}  // upcast()
-	
+       
+        btAlignedObjectArray<btPersistentManifold*> m_manifolds;
+        int houObjectId;
+       
+       
+        // CONSTRUCTORS
+        sim_btRigidBody( const btRigidBody::btRigidBodyConstructionInfo& constructionInfo ) : btRigidBody( constructionInfo )
+        {
+                houObjectId = -1;
+        }  // CONSTRUCTOR
+       
+        sim_btRigidBody( btScalar mass, btMotionState* motionState, btCollisionShape* collisionShape, const btVector3& localInertia=btVector3(0,0,0) )
+                : btRigidBody( mass, motionState, collisionShape, localInertia )
+        {
+                houObjectId = -1;
+        }  // CONSTRUCTOR
+       
+       
+        static const sim_btRigidBody* upcast(const btCollisionObject* colObj)
+        {
+                if ( colObj->getInternalType() == btCollisionObject::CO_RIGID_BODY )
+                        return (const sim_btRigidBody*)colObj;
+                return 0;
+        }  // upcast()
+       
+        static sim_btRigidBody* upcast(btCollisionObject* colObj)
+        {
+                if ( colObj->getInternalType() == btCollisionObject::CO_RIGID_BODY )
+                        return (sim_btRigidBody*)colObj;
+                return 0;
+        }  // upcast()
+       
 
 };  // class sim_btRigidBody
 // *************************************************************************** //
@@ -76,12 +75,12 @@ public:
 
 
 typedef struct bulletBodystr {
-	UT_String type;
-	int isStatic;
-	int isSet;
-	int dopId;
-	sim_btRigidBody* bodyId;
-	UT_BoundingBox bbox;
+        UT_String type;
+        int isStatic;
+        int isSet;
+        int dopId;
+        sim_btRigidBody* bodyId;
+        UT_BoundingBox bbox;
 } bulletBody;
 
 
@@ -110,50 +109,63 @@ typedef struct bulletBodystr {
 //#define GEO_REP_PITTON		"pitton"
 //#define GEO_REP_SPHEREPACK	"spherepack"
 
-
+//#define SIM_NAME_CHANGE_THRESHOLDS	"change_thresholds"
+#define SIM_NAME_LINEAR_SLEEP_THRESHOLD		"linear_sleep_threshold"
+#define SIM_NAME_ANGULAR_SLEEP_THRESHOLD	"angular_sleep_threshold"
+#define SIM_NAME_ISKINEMATIC				"is_kinematic"
 //#define DO_SNOW_STUFF   1
 
 
 
 
 class SIM_SnowBulletData : public SIM_Data,
-				public SIM_OptionsUser
+                                public SIM_OptionsUser
 {
-	public:
-		GETSET_DATA_FUNCS_S(SIM_NAME_GEO_REP, GeoRep);
-		GETSET_DATA_FUNCS_B(SIM_NAME_GEO_TRI, GeoTri);
-		GETSET_DATA_FUNCS_B(SIM_NAME_GEO_CONVEX, GeoConvex);
-		GETSET_DATA_FUNCS_V3(SIM_NAME_PRIM_T, PrimT);
-		GETSET_DATA_FUNCS_V3(SIM_NAME_PRIM_R, PrimR);
-		GETSET_DATA_FUNCS_V3(SIM_NAME_PRIM_S, PrimS);
-		GETSET_DATA_FUNCS_F(SIM_NAME_PRIM_RADIUS, PrimRadius);
-		GETSET_DATA_FUNCS_F(SIM_NAME_PRIM_LENGTH, PrimLength);
-		GETSET_DATA_FUNCS_F(SIM_NAME_COLLISION_MARGIN, CollisionMargin);
-		GETSET_DATA_FUNCS_B(SIM_NAME_PRIM_AUTOFIT, Autofit);
-		GETSET_DATA_FUNCS_I(SIM_NAME_PRIM_AUTOFIT_METHOD, AutofitMethod);
+        public:
+                GETSET_DATA_FUNCS_S(SIM_NAME_GEO_REP, GeoRep);
+                GETSET_DATA_FUNCS_B(SIM_NAME_GEO_TRI, GeoTri);
+                GETSET_DATA_FUNCS_B(SIM_NAME_GEO_CONVEX, GeoConvex);
+                GETSET_DATA_FUNCS_V3(SIM_NAME_PRIM_T, PrimT);
+                GETSET_DATA_FUNCS_V3(SIM_NAME_PRIM_R, PrimR);
+                GETSET_DATA_FUNCS_V3(SIM_NAME_PRIM_S, PrimS);
+                GETSET_DATA_FUNCS_F(SIM_NAME_PRIM_RADIUS, PrimRadius);
+                GETSET_DATA_FUNCS_F(SIM_NAME_PRIM_LENGTH, PrimLength);
+                GETSET_DATA_FUNCS_F(SIM_NAME_COLLISION_MARGIN, CollisionMargin);
+                GETSET_DATA_FUNCS_B(SIM_NAME_PRIM_AUTOFIT, Autofit);
+                GETSET_DATA_FUNCS_I(SIM_NAME_PRIM_AUTOFIT_METHOD, AutofitMethod);
+		
+				//Added by Chris 2010-06-07 //
+                //GETSET_DATA_FUNCS_B(SIM_NAME_CHANGE_THRESHOLDS, ChangeSleepThresholds);  // Indicates if user wants to use custom sleeping thresholds, rather than the default ones.
+                GETSET_DATA_FUNCS_F(SIM_NAME_LINEAR_SLEEP_THRESHOLD, LinearSleepThreshold);       // Sets the linear sleeping threshold value
+				GETSET_DATA_FUNCS_F(SIM_NAME_ANGULAR_SLEEP_THRESHOLD, AngularSleepThreshold);       // Sets the linear sleeping threshold value
+                // *********************** //
+                
+                //Added by SRH 2010-06-10//
+                GETSET_DATA_FUNCS_B(SIM_NAME_ISKINEMATIC, IsKinematic);	//Indicates whether or not bullet should re-evaluate the static geometry; i.e. if you have animated static meshes.
+                
+                
+                static const char* getName();
 
-		static const char* getName();
+        protected:
+                // This ensures that this data is always kept in RAM, even when
+                // the cache runs out of space and writes out the simulation
+                // step to disk. This is necessary because the Bullet data can't
+                // be written to disk.
+                virtual bool getCanBeSavedToDiskSubclass() const
+                        { return false; }
 
-	protected:
-		// This ensures that this data is always kept in RAM, even when
-		// the cache runs out of space and writes out the simulation
-		// step to disk. This is necessary because the Bullet data can't
-		// be written to disk.
-		virtual bool getCanBeSavedToDiskSubclass() const
-			{ return false; }
+        explicit             SIM_SnowBulletData(const SIM_DataFactory *factory);
+        virtual             ~SIM_SnowBulletData();
 
-	explicit             SIM_SnowBulletData(const SIM_DataFactory *factory);
-	virtual             ~SIM_SnowBulletData();
-
-	private:
-		static const SIM_DopDescription     *getSnowBulletDataDopDescription();
-	
-		DECLARE_STANDARD_GETCASTTOTYPE();
-		DECLARE_DATAFACTORY(SIM_SnowBulletData,
-				SIM_Data,
-				"Bullet Snow Data",
-				getSnowBulletDataDopDescription()
-			);
+        private:
+                static const SIM_DopDescription     *getSnowBulletDataDopDescription();
+       
+                DECLARE_STANDARD_GETCASTTOTYPE();
+                DECLARE_DATAFACTORY(SIM_SnowBulletData,
+                                SIM_Data,
+                                "Bullet Snow Data",
+                                getSnowBulletDataDopDescription()
+                        );
 };
 #endif
 
@@ -172,56 +184,57 @@ class SIM_SnowBulletData : public SIM_Data,
 
 
 class SIM_SnowNeighborData : public SIM_Data,
-				public SIM_OptionsUser
+                                public SIM_OptionsUser
 {
-	public:
-		GETSET_DATA_FUNCS_S(SIM_NAME_GEO_NEIGHBORS, GeoNeighbors);
+        public:
+                GETSET_DATA_FUNCS_S(SIM_NAME_GEO_NEIGHBORS, GeoNeighbors);
 
-		static const char* getName();
-		
-		// Data added to this array will be used for multiple data records.
-		// Added by SRH 2010-05-29 //
-		int getNumNeighborIds() const				// For implementing multiple data records
-			{ return neighborIds.entries(); }
-    	float getNeighborId( int i ) const			// For implementing multiple data records
-    		{ return neighborIds(i); }
-	    void appendNeighborId( float id )				// For implementing multiple data records
-	    	{ neighborIds.append(id); }
-	    void resetNeighborIds()
-	    	{ neighborIds.resize(0); }
-	    // *********************** //
+                static const char* getName();
+               
+                // Data added to this array will be used for multiple data records.
+                // Added by SRH 2010-05-29 //
+                int getNumNeighborIds() const                           // For implementing multiple data records
+                    { return neighborIds.entries(); }
+                float getNeighborId( int i ) const                      // For implementing multiple data records
+                    { return neighborIds(i); }
+                void appendNeighborId( float id )                           // For implementing multiple data records
+                    { neighborIds.append(id); }
+                void resetNeighborIds()
+                    { neighborIds.resize(0); }
+            // *********************** //
 
 
-	protected:
-		// This ensures that this data is always kept in RAM, even when
-		// the cache runs out of space and writes out the simulation
-		// step to disk. This is necessary because the Bullet data can't
-		// be written to disk.
-		virtual bool getCanBeSavedToDiskSubclass() const
-			{ return false; }
-		
-		// This multiple records data should also be persisted so custom implementations 
-		//   of saveSubclass() and loadSubclass() should be provided
-		// Added by SRH 2010-05-29
-		virtual void saveSubclass(ostream &os) const;			// For implementing multiple data records
-		virtual bool loadSubclass(UT_IStream &is);				// For implementing multiple data records
-		virtual SIM_Query *createQueryObjectSubclass() const;	// For implementing multiple data records
-		virtual void makeEqualSubclass(const SIM_Data *source);	// For implementing multiple data records
+        protected:
+                // This ensures that this data is always kept in RAM, even when
+                // the cache runs out of space and writes out the simulation
+                // step to disk. This is necessary because the Bullet data can't
+                // be written to disk.
+                virtual bool getCanBeSavedToDiskSubclass() const
+                        { return false; }
+               
+                // This multiple records data should also be persisted so custom implementations
+                //   of saveSubclass() and loadSubclass() should be provided
+                // Added by SRH 2010-05-29
+                virtual void saveSubclass(ostream &os) const;                   // For implementing multiple data records
+                virtual bool loadSubclass(UT_IStream &is);                              // For implementing multiple data records
+                virtual SIM_Query *createQueryObjectSubclass() const;   // For implementing multiple data records
+                virtual void makeEqualSubclass(const SIM_Data *source); // For implementing multiple data records
 
-		explicit             SIM_SnowNeighborData(const SIM_DataFactory *factory);
-		virtual             ~SIM_SnowNeighborData();
 
-	private:
-		UT_FloatArray neighborIds;
-		
-		static const SIM_DopDescription     *getSnowNeighborDataDopDescription();
-	
-		DECLARE_STANDARD_GETCASTTOTYPE();
-		DECLARE_DATAFACTORY(SIM_SnowNeighborData,
-				SIM_Data,
-				"NeighborData",
-				getSnowNeighborDataDopDescription()
-			);
+                explicit             SIM_SnowNeighborData(const SIM_DataFactory *factory);
+                virtual             ~SIM_SnowNeighborData();
+
+        private:
+                UT_FloatArray neighborIds;
+               
+                static const SIM_DopDescription     *getSnowNeighborDataDopDescription();
+       
+                DECLARE_STANDARD_GETCASTTOTYPE();
+                DECLARE_DATAFACTORY(SIM_SnowNeighborData,
+                                SIM_Data,
+                                "NeighborData",
+                                getSnowNeighborDataDopDescription()
+                        );
 };
 // ********************************************************************************* //
 
@@ -233,27 +246,27 @@ class SIM_SnowNeighborData : public SIM_Data,
 
 
 class SIM_SnowSolverBulletState {
-	public:
-		int  refCount;
-		std::map<int, bulletBody> *m_bulletBodies;
-		// ADDED BY SRH 2010-03-31 //
-		std::map<int, bulletBody> *m_bulletAffectors;
-		// *********************** //
-		btBroadphaseInterface*	m_broadphase;
-		btCollisionDispatcher*	m_dispatcher;
-		btConstraintSolver*	m_solver;
+        public:
+                int  refCount;
+                std::map<int, bulletBody> *m_bulletBodies;
+                // ADDED BY SRH 2010-03-31 //
+                std::map<int, bulletBody> *m_bulletAffectors;
+                // *********************** //
+                btBroadphaseInterface*  m_broadphase;
+                btCollisionDispatcher*  m_dispatcher;
+                btConstraintSolver*     m_solver;
 #ifdef DO_SNOW_STUFF
-		shGranularDiscreteDynamicsWorld* m_dynamicsWorld;
+                shGranularDiscreteDynamicsWorld* m_dynamicsWorld;
 #else
-        	btDiscreteDynamicsWorld* m_dynamicsWorld;
+                btDiscreteDynamicsWorld* m_dynamicsWorld;
 #endif
-		btDefaultCollisionConfiguration* m_collisionConfiguration;
-		SIM_SnowSolverBulletState(); 
-		~SIM_SnowSolverBulletState();
-		void addReference();
-		void removeReference();
-		void initSystem( );
-		void cleanSystem(  );
+                btDefaultCollisionConfiguration* m_collisionConfiguration;
+                SIM_SnowSolverBulletState();
+                ~SIM_SnowSolverBulletState();
+                void addReference();
+                void removeReference();
+                void initSystem( );
+                void cleanSystem(  );
 };
 
 
@@ -263,55 +276,57 @@ class SIM_SnowSolverBulletState {
 // ADDED BY SRH 2010-06-02 //
 #define SIM_NAME_COMPUTE_IMPACTS     "compute_impacts"
 #define SIM_NAME_SUBSTEPS            "substeps"
+
 // *********************** //
 
 
 class SIM_SnowSolverBullet : public SIM_Solver, public SIM_OptionsUser
 {
-	public:
-		SIM_SnowSolverBulletState       *state;
-		
-		// ADDED BY SRH 2010-06-02 //
-		GETSET_DATA_FUNCS_B(SIM_NAME_COMPUTE_IMPACTS, ComputeImpacts);	// Mark whether or not Bullet should compute impacts data (default on)
-		GETSET_DATA_FUNCS_I(SIM_NAME_SUBSTEPS, Substeps);	// Set number of substeps the Bullet Engine will take, additional to the DOP Network's substeps setting
-		// *********************** //
+        public:
+                SIM_SnowSolverBulletState       *state;
+               
+                // ADDED BY SRH 2010-06-02 //
+                GETSET_DATA_FUNCS_B(SIM_NAME_COMPUTE_IMPACTS, ComputeImpacts);  // Mark whether or not Bullet should compute impacts data (default on)
+                GETSET_DATA_FUNCS_I(SIM_NAME_SUBSTEPS, Substeps);       // Set number of substeps the Bullet Engine will take, additional to the DOP Network's substeps setting
+                // *********************** //
+                
 
-	protected:
-		virtual void makeEqualSubclass(const SIM_Data *src)
-		{
-			//cout<<"makeequalSubclass called, start "<<endl;
-			SIM_SnowSolverBullet *world;
-			world = const_cast<SIM_SnowSolverBullet *> SIM_DATA_CASTCONST(src, SIM_SnowSolverBullet);
-			if( world && world->state )
-			{
-				state = world->state;
-				state->addReference();
-			}
-		}
+                
 
-		explicit SIM_SnowSolverBullet(const SIM_DataFactory *factory);
-		virtual ~SIM_SnowSolverBullet();
-		virtual SIM_Solver::SIM_Result solveObjectsSubclass(SIM_Engine &engine,
-			SIM_ObjectArray &objects,
-			SIM_ObjectArray &newobjects,
-			SIM_ObjectArray &feedbacktoobjects,
-			const SIM_Time &timestep);
-		virtual std::map< int, bulletBody >::iterator addBulletBody(SIM_Object *currObject);
-		virtual void removeDeadBodies(SIM_Engine &engine);
-		virtual void processSubData(SIM_Object *currObject, bulletBody &body);
+        protected:
+                virtual void makeEqualSubclass(const SIM_Data *src)
+                {
+                        //cout<<"makeequalSubclass called, start "<<endl;
+                        SIM_SnowSolverBullet *world;
+                        world = const_cast<SIM_SnowSolverBullet *> SIM_DATA_CASTCONST(src, SIM_SnowSolverBullet);
+                        if( world && world->state )
+                        {
+                                state = world->state;
+                                state->addReference();
+                        }
+                }
 
-	private:
-		static const SIM_DopDescription	*getSolverBulletDopDescription(); 
-	
-		DECLARE_STANDARD_GETCASTTOTYPE();
-		DECLARE_DATAFACTORY(SIM_SnowSolverBullet,
-			SIM_Solver,
-			"Bullet Solver",
-			getSolverBulletDopDescription());
+                explicit SIM_SnowSolverBullet(const SIM_DataFactory *factory);
+                virtual ~SIM_SnowSolverBullet();
+                virtual SIM_Solver::SIM_Result solveObjectsSubclass(SIM_Engine &engine,
+                        SIM_ObjectArray &objects,
+                        SIM_ObjectArray &newobjects,
+                        SIM_ObjectArray &feedbacktoobjects,
+                        const SIM_Time &timestep);
+                virtual std::map< int, bulletBody >::iterator addBulletBody(SIM_Object *currObject);
+                virtual void removeDeadBodies(SIM_Engine &engine);
+                virtual void processSubData(SIM_Object *currObject, bulletBody &body);
+
+        private:
+                static const SIM_DopDescription *getSolverBulletDopDescription();
+       
+                DECLARE_STANDARD_GETCASTTOTYPE();
+                DECLARE_DATAFACTORY(SIM_SnowSolverBullet,
+                        SIM_Solver,
+                        "Bullet Solver",
+                        getSolverBulletDopDescription());
 };
 
 
 #endif
-
-
 
