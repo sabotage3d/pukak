8941ed1642e5d2eb18d6260be8fe064817f68b76
seth.holladay <seth.holladay@b0b181a8-020e-11df-8f7b-bfe16de6f99b>
2010-06-19 23:59:12 +0000

    Adding SIM_SolverBullet_to_houdinifx.py, a script that copies the SIM_SnowSolverBullet.h and SIM_SnowSolverBullet.cpp files to the houdinifx group.

---
+++ houdini/SIM_SolverBullet_to_houdinifx.py
@@ -0,0 +1,31 @@
+import getopt
+import os
+import shutil
+import sys
+
+
+def usage():
+    print( 'Usage: SIM_SolverBullet_to_houdinifx.py' )
+
+
+houdinifxPath = 'J:/groups/houdinifx/tools/bullet'
+if not os.path.exists( houdinifxPath ):
+    print( 'Error: Cannot find houdinifx group folder' )
+    sys.exit(1)
+
+houdiniPath = ''
+simSolverBulletSrcPath = ''
+try:    # If you have access to the Houdini environment variables, find the files relative to HFS
+    houdiniPath = os.environ['HFS']
+    simSolverBulletSrcPath = houdiniPath + '\\toolkit\\SIM_SolverBullet-0.11\\src'
+    simSolverBulletSrcPath = simSolverBulletSrcPath.replace( '\\', '/' )
+except( KeyError ):   # Otherwise, find the files relative to the current working directory
+    houdiniPath = os.getcwd()
+    simSolverBulletSrcPath = houdiniPath + '\\src'
+    simSolverBulletSrcPath = simSolverBulletSrcPath.replace( '\\', '/' )
+
+simSolverBullet_h = simSolverBulletSrcPath + '/SIM_SnowSolverBullet.h'
+simSolverBullet_cpp = simSolverBulletSrcPath + '/SIM_SnowSolverBullet.cpp'
+
+shutil.copy( simSolverBullet_h, houdinifxPath )
+shutil.copy( simSolverBullet_cpp, houdinifxPath )
