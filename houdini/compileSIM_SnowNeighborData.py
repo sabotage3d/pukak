30c10644733ba0b9705a4e84ad7379e548f6452a
seth.holladay <seth.holladay@b0b181a8-020e-11df-8f7b-bfe16de6f99b>
2010-12-15 23:27:40 +0000

    Still setting up the SIM_SnowNeighborData class in its own file so that it compiles into its own DOP node.  It currently does not compile, but all the files and compiling files are set up.

---
+++ houdini/compileSIM_SnowNeighborData.py
@@ -0,0 +1,42 @@
+import getopt
+import os
+import shutil
+import sys
+
+
+def usage():
+    print( 'Usage: compileDOP_GroupCollidingObjects.py [options]' )
+    print( '    -h, --help           Print this help.' )
+    
+
+doCopy = False
+
+try:                                
+    opts, args = getopt.getopt( sys.argv[1:], "hc", ["help"] )
+except getopt.GetoptError:
+    print( 'Bad arg' )
+    usage()
+    sys.exit(2)
+    
+for (opt, arg) in opts:
+    if opt in ( '-h', '--help' ):
+        usage()
+        sys.exit(2)
+
+houdiniPath = None
+
+try:
+    houdiniPath = os.environ['HFS']
+except( KeyError ):
+    print( 'Error: Cannot find environment variable $HFS.  Run this script from SideFX "Command Line Tools."' )
+    sys.exit(1)
+
+# Go to the directory containing the SOP_FractalGrowth code
+simSnowNeighborDataSrcPath = houdiniPath + '/toolkit/SIM_SnowBulletSolver/src'
+simSnowNeighborDataSrcPath = simSnowNeighborDataSrcPath.replace( '\\', '/' )
+os.chdir( simSnowNeighborDataSrcPath )
+
+#hcustomCommand = 'hcustom -i ../lib SOP_FractalGrowth.cpp'
+hcustomCommand = 'hcustom SIM_SnowNeighborData.cpp'
+os.system( hcustomCommand )
+
