0694b8d6663a27af6892a280fae0ff3366874db1
seth.holladay <seth.holladay@b0b181a8-020e-11df-8f7b-bfe16de6f99b>
2011-04-21 15:07:55 +0000

    Adding DOP_TransferObjidsToMesh, which transfers granule objids to their corresponding vertices on the mesh.

---
+++ dopgroupgranules/compileDOP_GroupInteriorGranuleNeighbors.py
@@ -0,0 +1,41 @@
+import getopt
+import os
+import shutil
+import sys
+
+
+def usage():
+    print( 'Usage: compileDOP_GroupInteriorGranuleNeighbors.py [options]' )
+    print( '    -h, --help           Print this help.' )
+    
+
+doCopy = False
+
+try:                                
+    opts, args = getopt.getopt( sys.argv[1:], "hc", ["help"] )
+except getopt.GetoptError:
+    print( 'Bad arg' )
+    usage()
+    sys.exit(2)
+    
+for (opt, arg) in opts:
+    if opt in ( '-h', '--help' ):
+        usage()
+        sys.exit(2)
+
+houdiniPath = None
+
+try:
+    houdiniPath = os.environ['HFS']
+except( KeyError ):
+    print( 'Error: Cannot find environment variable $HFS.  Run this script from SideFX "Command Line Tools."' )
+    sys.exit(1)
+
+# Go to the directory containing the SOP_FractalGrowth code
+dopSrcPath = houdiniPath + '/toolkit/DOP_GroupGranules/src'
+dopSrcPath = dopSrcPath.replace( '\\', '/' )
+os.chdir( dopSrcPath )
+
+hcustomCommand = 'hcustom DOP_GroupInteriorGranuleNeighbors.cpp'
+os.system( hcustomCommand )
+
